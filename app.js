159619d6e6bdc8c89ee58ce60a77c806f59d2245
Jose Torres <jt227b@gmail.com>
2020-01-18 10:43:43 -0600

    set counter to 0 points

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores, roundScores, activePlayer;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
@@ -20,7 +20,11 @@ activePlayer = 1;
 
 document.querySelector('.dice').style.display = 'none';
 
-
+// Sets counter from 19 85 to 0
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
 
