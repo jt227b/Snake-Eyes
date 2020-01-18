4f22dc09664f7bc1b3a12acf6775c4069d13206e
Jose Torres <jt227b@gmail.com>
2020-01-18 14:00:09 -0600

    Added Winner feature to the game

--- app.js
+++ app.js
@@ -57,7 +57,15 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
   // 3. Check if player won the game
-
+  if (scores[activePlayer] >= 100) {
+    document.querySelector('#name-' + activePlayer).textContent = "Winner!";
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  } else {
+    // Next Player
+    nextPlayer();
+  }
 
   // Next Player
   nextPlayer();
